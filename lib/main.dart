632aba70ad719c4e84e20258a6b9080b739665d6
mostafaadawy <mostafa_adawy@ymail.com>
2021-11-12 14:09:25 +0200

    conditioning widget outputting

--- lib/main.dart
+++ lib/main.dart
@@ -55,12 +55,12 @@ class _MyAppState extends State<MyApp>{
         title: Text('My First App'),
       ),
 
-      body: Column(children: [
+      body: _questionIndex < questions.length? Column(children: [
         Question(questions[_questionIndex]['questionText'] as String),
         ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
           return Answer(_answerQuestion,answer);
         }).toList(),
-      ],),
+      ],) : Center(child: Text('No more questions')),
     ),);
  
   }
